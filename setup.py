34c2faf5191d8f34c7d9992fc4f4220b7a1d98b3
Sarath-peddireddy <sarathpeddireddy93477@gmail.com>
2025-02-17 21:04:48 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name = "Medical ChatBot",
+    version = '0.0.0',
+    author='sarath peddireddy',
+    author_email='sarathpeddireddy93477@gmail.com',
+    packages =find_packages(),
+    install_requires= []
+    )
